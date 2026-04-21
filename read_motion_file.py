73340e525520c2387de678368fad0989c00d58e1
jennyhto <jenny@stud.ntnu.no>
2026-04-21 10:52:23 +0200

    fikset feil i read motion file

--- read_motion_file.py
+++ read_motion_file.py
@@ -14,32 +14,35 @@ def readfile_motion(filename):
   x_arr = np.array([])
   y_arr = np.array([])
 
+  def clean_number(s):
+        s = s.strip()
+        s = s.replace('−', '-')  
+        s = s.replace('–', '-')   
+        s = s.replace('—', '-')   
+        s = re.sub(r"[^\d\.\-Ee+]", "", s)
+        return float(s)
+
   #skip header line and split by comma before setting correct numbers together to update list
   for line in lines[2:]:
       parts = line.strip().split(",")
 
       t = float(parts[0] + "." + parts[1])
+      
       #If data is missing at some timesteps
       if len(parts)==3:
         x = math.nan
-      elif len(parts)==4:
+      elif len(parts)>=4:
         x = parts[2] + "." + parts[3]
-      elif len(parts)==5:
-         y = math.nan
+      
+      if len(parts)==5:
+        y = math.nan
       elif len(parts)==6:
         y = parts[4] + "." + parts[5]
-
-      #fix encoding for x and y values
-      x = x.replace('−', '-')
-      x = re.sub(r"[^\d\.\-Ee+]", "", x)
-      if len(parts)==6:
-        y = y.replace('−', '-')
-        y = re.sub(r"[^\d\.\-Ee+]", "", y)
-
+      
       #add values to arrays
       t_arr = np.append(t_arr, t)
-      x_arr = np.append(x_arr, float(x))
-      y_arr = np.append(y_arr, float(y))
+      x_arr = np.append(x_arr, clean_number(x))
+      y_arr = np.append(y_arr, clean_number(y))
   return t_arr, x_arr, y_arr
 
 
